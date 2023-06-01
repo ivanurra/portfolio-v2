1c362573f37d0e86799b5e29b9781fc70ab7c469
Ivan Urra <ivan.urra.wd@gmail.com>
2023-06-01 08:11:04 +0200

    WIP

--- modules/Hero/Hero.jsx
+++ modules/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
   return (
     <div>
-      <p class="not-italic">Hi, my name is</p>
+      <p className="not-italic">Hi, my name is</p>
       <p class="not-italic">Iván Urra</p>
     </div>
   );
