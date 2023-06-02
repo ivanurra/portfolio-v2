6a620f0e6f4eac9d4b6b28bbb8bae6c5396c3fe0
Ivan Urra <ivan.urra.wd@gmail.com>
2023-06-02 07:55:07 +0200

    WIP

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,5 @@
-"use client";
-
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-
-function Header() {
+export const Header = () => {
   return (
-    <Navbar
-      bg="dark"
-      variant="dark"
-      expand="lg"
-      className="fixed-top color-nav shadow-navbar"
-    >
-      <Navbar.Brand href="/">
-        ivanurra<span className="color-number">.dev</span>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto mt-2 ">
-          <Nav.Link href="/">
-            <span className="color-number">01.</span>About
-          </Nav.Link>
-          <Nav.Link href="/">
-            <span className="color-number">01.</span>Portfolio
-          </Nav.Link>
-          <Nav.Link href="/">
-            <span className="color-number">02.</span>Contact
-          </Nav.Link>
-          <Nav.Link href="/">Resume</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+    <div>Navbar</div>
+  )
 }
-
-export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import { About, Contact, Hero, Jobs, Portfolio } from "../../modules";
-import Header from "../../components/Header";
-import { Footer } from "../../components/Footer";
-
+import { Header, Footer } from "../../components/";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>
+      <Header />
       <Hero />
       <About />
       <Jobs />
