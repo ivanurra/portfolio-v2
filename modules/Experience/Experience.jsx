00c6deaa26030aaa081d035bfdcc7290b86a46cb
Ivan Urra <ivan.urra.wd@gmail.com>
2023-06-24 11:27:26 +0200

    WIP

--- modules/Experience/Experience.jsx
+++ modules/Experience/Experience.jsx
@@ -1,219 +1,178 @@
 import { useState } from "react";
 import { ButtonStack } from "../../components";
-import Arrow from "../../components/icons/arrow";
 
 export const Experience = () => {
-  const [isGridHoveredJobOne, setIsGridHoveredJobOne] = useState(false);
-  const [isGridHoveredJobTwo, setIsGridHoveredJobTwo] = useState(false);
-  const [isGridHoveredJobThree, setIsGridHoveredJobThree] = useState(false);
-  const [isGridHoveredJobFour, setIsGridHoveredJobFour] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
     <div>
       <div className="mb-12 md:p-8">
-        <a
-          href="https://www.philotech.net/"
-          className="cursor-pointer"
-          target="_blank"
-          onMouseEnter={() => setIsGridHoveredJobOne(true)}
-          onMouseLeave={() => setIsGridHoveredJobOne(false)}
-        >
-          <p className="text-xl mb-4 font-bold block lg:hidden">Experience</p>
-          <div
-            className={`md:grid grid-cols-4 ${
-              isGridHoveredJobOne ? "bg-background-secondary" : ""
-            } `}
-          >
-            {/* 1st Column */}
-            <div className="md:col-span-1">
-              <p className="text-sm text-font-color-secondary">
-                JUN 2023 - Present
-              </p>
-            </div>
+        <p className="text-xl mb-4 font-bold block lg:hidden">Experience</p>
+        <div className={`md:grid grid-cols-4  `}>
+          {/* 1st Column */}
+          <div className="md:col-span-1">
+            <p className="text-sm text-font-color-secondary">
+              JUN 2023 - Present
+            </p>
+          </div>
 
-            {/* 2nd Column */}
-            <div className="md:col-span-3">
-              <p
-                className={`mb-2 text-base ${
-                  isGridHoveredJobOne ? "text-green-primary font-bold" : ""
-                }`}
-              >
-                Philotech
-              </p>
-              <p className="mb-2 text-sm text-font-color-secondary">
-                DevOps Developer
-              </p>
-              <p className="text-sm text-font-color-secondary">
-                Provision of the infrastructure in a cloud environment for
-                microservices. Central contact person for application operation.
-                Implementation of changes and deployments. Troubleshooting and
-                analysis of logs.
-              </p>
-              {/* <div>
+          {/* 2nd Column */}
+          <div className="md:col-span-3">
+            <a
+              href="https://www.philotech.net/"
+              className="cursor-pointer flex flex-row hover:text-green-primary hover:fill-green-primary"
+              target="_blank"
+            >
+              <div>
+                <p className="mb-2 text-base ">Philotech</p>
+              </div>
+
+              <div className="ml-1 relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  className={`h-4 w-4 fill-current transition-transform ${
+                    isHovered ? "-translate-y-1" : ""
+                  }`}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <path d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"></path>
+                </svg>
+              </div>
+            </a>
+            <p className="mb-2 text-sm text-font-color-secondary">
+              DevOps Developer
+            </p>
+            <p className="text-sm text-font-color-secondary">
+              Provision of the infrastructure in a cloud environment for
+              microservices. Central contact person for application operation.
+              Implementation of changes and deployments. Troubleshooting and
+              analysis of logs.
+            </p>
+            {/* <div>
                 <ButtonStack stack="Docker" />
                 <ButtonStack stack="TailwindCSS" />
                 <ButtonStack stack="JavaScript" />
                 <ButtonStack stack="React" />
               </div> */}
-            </div>
           </div>
-        </a>
+        </div>
       </div>
       <div className="mb-12 md:p-8">
-        <a
-          href="https://secuoyas.com/"
-          className="cursor-pointer"
-          target="_blank"
-          onMouseEnter={() => setIsGridHoveredJobTwo(true)}
-          onMouseLeave={() => setIsGridHoveredJobTwo(false)}
-        >
-          <div
-            className={`md:grid grid-cols-4 ${
-              isGridHoveredJobTwo ? "bg-background-secondary" : ""
-            }`}
-          >
-            {/* 1st Column */}
-            <div className="md:col-span-1">
-              <p className="text-sm text-font-color-secondary">
-                AUG 2021 - JUN 2023
-              </p>
-            </div>
+        <div className={`md:grid grid-cols-4 `}>
+          {/* 1st Column */}
+          <div className="md:col-span-1">
+            <p className="text-sm text-font-color-secondary">
+              AUG 2021 - JUN 2023
+            </p>
+          </div>
 
-            {/* 2nd Column */}
-            <div className="md:col-span-3">
-              <p
-                className={`mb-2 text-base ${
-                  isGridHoveredJobTwo ? "text-green-primary font-bold" : ""
-                }`}
-              >
+          {/* 2nd Column */}
+          <div className="md:col-span-3">
+            <a
+              href="https://secuoyas.com/"
+              className="cursor-pointer"
+              target="_blank"
+            >
+              <p className={`mb-2 text-base hover:text-green-primary`}>
                 Secuoyas
-                <Arrow />
               </p>
-              <p className="mb-2 text-sm text-font-color-secondary">
-                Full Stack Web Developer
-              </p>
-              <p className="text-sm text-font-color-secondary">
-                Write modern, performant, clean and maintainable code focused on
-                Griddo.io, the company's own CMS, which is used by the best
-                universities in the country. Work with a variety of different
-                languages and frameworks such as JavaScript, React and Gatsby.
-                Communicate and collaborate with multi-disciplinary teams of
-                engineers, designers, and stakeholders on a daily basis.
-              </p>
-              <div>
-                <ButtonStack stack="JavaScript" />
-                <ButtonStack stack="React" />
-                <ButtonStack stack="Gatsby" />
-              </div>
+            </a>
+            <p className="mb-2 text-sm text-font-color-secondary">
+              Full Stack Web Developer
+            </p>
+            <p className="text-sm text-font-color-secondary">
+              Write modern, performant, clean and maintainable code focused on
+              Griddo.io, the company's own CMS, which is used by the best
+              universities in the country. Work with a variety of different
+              languages and frameworks such as JavaScript, React and Gatsby.
+              Communicate and collaborate with multi-disciplinary teams of
+              engineers, designers, and stakeholders on a daily basis.
+            </p>
+            <div>
+              <ButtonStack stack="JavaScript" />
+              <ButtonStack stack="React" />
+              <ButtonStack stack="Gatsby" />
             </div>
           </div>
-        </a>
+        </div>
       </div>
       <div className="mb-12 md:p-8">
-        <a
-          href="https://www.auro.travel/"
-          className="cursor-pointer"
-          target="_blank"
-          onMouseEnter={() => setIsGridHoveredJobThree(true)}
-          onMouseLeave={() => setIsGridHoveredJobThree(false)}
-        >
-          <div
-            className={`md:grid grid-cols-4 ${
-              isGridHoveredJobThree ? "bg-background-secondary" : ""
-            }`}
-          >
-            {/* 1st Column */}
-            <div className="md:col-span-1">
-              <p className="text-sm text-font-color-secondary">
-                MAR 2021 - AUG 2021
-              </p>
-            </div>
+        <div className={`md:grid grid-cols-4 `}>
+          {/* 1st Column */}
+          <div className="md:col-span-1">
+            <p className="text-sm text-font-color-secondary">
+              MAR 2021 - AUG 2021
+            </p>
+          </div>
 
-            {/* 2nd Column */}
-            <div className="md:col-span-3">
-              <p
-                className={`mb-2 text-base ${
-                  isGridHoveredJobThree ? "text-green-primary font-bold" : ""
-                }`}
-              >
-                Auro
-              </p>
-              <p className="mb-2 text-sm text-font-color-secondary">
-                Full Stack Web Developer
-              </p>
-              <p className="text-sm text-font-color-secondary">
-                Built and shipped the new Auro website. Worked closely with
-                designer and commercial team to develop the website using
-                JavaScript, Vue, Nuxt and Vuetify. Developed and maintained code
-                for in-house projects primarily.
-              </p>
-              <div>
-                <ButtonStack stack="JavaScript" />
-                <ButtonStack stack="Vue" />
-                <ButtonStack stack="Nuxt" />
-                <ButtonStack stack="Vuetify" />
-              </div>
+          {/* 2nd Column */}
+          <div className="md:col-span-3">
+            <a
+              href="https://www.auro.travel/"
+              className="cursor-pointer"
+              target="_blank"
+            >
+              <p className={`mb-2 text-base hover:text-green-primary`}>Auro</p>
+            </a>
+            <p className="mb-2 text-sm text-font-color-secondary">
+              Full Stack Web Developer
+            </p>
+            <p className="text-sm text-font-color-secondary">
+              Built and shipped the new Auro website. Worked closely with
+              designer and commercial team to develop the website using
+              JavaScript, Vue, Nuxt and Vuetify. Developed and maintained code
+              for in-house projects primarily.
+            </p>
+            <div>
+              <ButtonStack stack="JavaScript" />
+              <ButtonStack stack="Vue" />
+              <ButtonStack stack="Nuxt" />
+              <ButtonStack stack="Vuetify" />
             </div>
           </div>
-        </a>
+        </div>
       </div>
       <div className="mb-20 md:p-8">
-        <a
-          href="https://www.iberostar.com/"
-          className="cursor-pointer"
-          target="_blank"
-          onMouseEnter={() => setIsGridHoveredJobFour(true)}
-          onMouseLeave={() => setIsGridHoveredJobFour(false)}
-        >
-          <div
-            className={`md:grid grid-cols-4 ${
-              isGridHoveredJobFour ? "bg-background-secondary" : ""
-            } `}
-          >
-            {/* 1st Column */}
-            <div className="md:col-span-1">
-              <p className="text-sm text-font-color-secondary">
-                MAR 2019 - MAR 2021
-              </p>
-            </div>
+        <div className={`md:grid grid-cols-4  `}>
+          {/* 1st Column */}
+          <div className="md:col-span-1">
+            <p className="text-sm text-font-color-secondary">
+              MAR 2019 - MAR 2021
+            </p>
+          </div>
 
-            {/* 2nd Column */}
-            <div className="md:col-span-3">
-              <p
-                className={`mb-2 text-base ${
-                  isGridHoveredJobFour ? "text-green-primary font-bold" : ""
-                }`}
-              >
+          {/* 2nd Column */}
+          <div className="md:col-span-3">
+            <a
+              href="https://www.iberostar.com/"
+              className="cursor-pointer"
+              target="_blank"
+            >
+              <p className={`mb-2 text-base hover:text-green-primary`}>
                 Iberostar
               </p>
-              <p className="mb-2 text-sm text-font-color-secondary">
-                IT Support Technician
-              </p>
-              <p className="text-sm text-font-color-secondary">
-                Performed high-level support to more than 50 hotels in EMEA
-                countries. Facilitated Cloud software installation and
-                configuration in updates or openings of new hotels. As a team,
-                we were able to reduce helpdesk tickets by 60%.
-              </p>
-            </div>
+            </a>
+            <p className="mb-2 text-sm text-font-color-secondary">
+              IT Support Technician
+            </p>
+            <p className="text-sm text-font-color-secondary">
+              Performed high-level support to more than 50 hotels in EMEA
+              countries. Facilitated Cloud software installation and
+              configuration in updates or openings of new hotels. As a team, we
+              were able to reduce helpdesk tickets by 60%.
+            </p>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
 };
-{
-  /* <svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="black"
-  class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-  aria-hidden="true"
->
-  <path
-    fill-rule="evenodd"
-    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-    clip-rule="evenodd"
-  ></path>
-</svg> */
-}
