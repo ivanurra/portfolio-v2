7925825a5233048c1b177617d5908de9a1f82172
Ivan Urra <ivan.urra.wd@gmail.com>
2023-06-24 10:45:41 +0200

    WIP

---
+++ components/icons/Arrow.js
@@ -0,0 +1,13 @@
+const Arrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      className="h-4 w-4 fill-current text-font-color-primary"
+    >
+      <path d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"></path>
+    </svg>
+  );
+};
+
+export default Arrow;
