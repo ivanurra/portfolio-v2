188caa7d3f4ac2dcce07cac57d60e7c42a3c7502
Ivan Urra <ivan.urra.wd@gmail.com>
2023-07-06 06:55:04 +0200

    WIP

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ import { About, Hero, Experience, Projects } from "../../modules";
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="md:w-2/4 md:h-screen p-12 md:fixed left-0 top-0 xl:pl-80 2xl:pl-96">
+      <div className="md:w-2/4 md:h-screen p-10 md:fixed left-0 top-0 xl:pl-80 2xl:pl-96">
         <Hero />
       </div>
 
-      <div className="md:w-2/4 p-12 ml-auto xl:pr-80 2xl:pr-96">
+      <div className="md:w-2/4 p-10 ml-auto xl:pr-80 2xl:pr-96">
         <section id="section1">
           <About />
         </section>
