40464022f713ac57f745a724e135b4f261a91e5f
Ivan Urra <ivan.urra.wd@gmail.com>
2023-07-27 06:32:44 +0200

    WIP

--- modules/About/About.jsx
+++ modules/About/About.jsx
@@ -5,15 +5,15 @@ export const About = () => {
         <p className="text-xl mb-4 font-bold block md:hidden">About</p>
         <p className="text-font-color-secondary">
           Hello! I'm Iván, a DevOps & Full Stack Web Developer. <br /> <br />I
-          enjoy{" "}
+          enjoy
           <span className="text-font-color-primary">
             creating things that live on the internet.
           </span>{" "}
           I am very excited about the possibilities of technology making life
-          simpler. <br /> <br />I value commitment, hard work, and motivation to{" "}
+          simpler. <br /> <br />I value commitment, hard work, and motivation to
           <span className="text-font-color-primary">
             learn something new every day.
-          </span>{" "}
+          </span>
           I love teamwork and helping others. I have worked for more than ten
           years in highly regulated IT environments.
         </p>
