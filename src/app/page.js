11a17f41dbf6a21ce790ee4225d636b0962745db
Ivan Urra <ivan.urra.wd@gmail.com>
2023-08-11 07:39:14 +0200

    WIP

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,6 @@ export default function Home() {
       <div className="md:w-2/4 md:h-screen p-10 md:fixed left-0 top-0 xl:pl-80 2xl:pl-96">
         <Hero />
       </div>
-
       <div className="md:w-2/4 p-10 ml-auto xl:pr-80 2xl:pr-96">
         <section id="section1">
           <About />
