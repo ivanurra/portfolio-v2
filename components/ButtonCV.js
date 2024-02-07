c21be289e5e9313170ede8971a918b39ca9bfe98
Ivan Urra <ivan.urra.wd@gmail.com>
2024-02-07 06:44:08 +0100

    wip

--- components/ButtonCV.js
+++ components/ButtonCV.js
@@ -1,7 +1,12 @@
 export const ButtonCV = ({ buttonCvText }) => {
   return (
     <div className="mt-14">
-      <a className="cta-resume" target="_blank" href="/resumeivanurra.pdf">
+      <a
+        className="cta-resume"
+        href="/resumeivanurra.pdf"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <button className="rounded-full bg-green-primary hover:bg-font-color-primary text-black w-32 h-9">
           {buttonCvText}
         </button>
