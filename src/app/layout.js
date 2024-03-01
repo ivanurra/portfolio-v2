bcdb5898088a27834387912e515aa3edebd9b641
Ivan Urra <ivan.urra.wd@gmail.com>
2024-03-01 07:21:11 +0100

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
   description: "Ivan Urra - DevOps & Full Stack Web Developer",
   image: "https://ivanurra.dev/mockup.png",
   site_name: "ivanurra.dev",
-  title: "Ivan Urra - DevOps & Full Stack Web Developer",
+  title: "ivanurra.dev",
   type: "website",
   url: "https://ivanurra.dev",
 };
